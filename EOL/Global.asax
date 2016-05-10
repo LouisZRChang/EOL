b99b3aa7cc7330e06fa6a3dcc2d5ae0e7af447ce
Louis <Louis.zr.chang@fubon.com>
2016-05-10 09:58:22 +0800

    First Commit

---
+++ EOL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EOL.MvcApplication" Language="C#" %>
---
+++ EOL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EOL.Startup))]
+namespace EOL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
